9178d217e7f3e57cd5fb3286fc4e263151d749bc
Bryan <bryan@bluebonnettech.com>
2026-05-09 01:42:18 +0000

    models: add denorm fields to Session for cross-env display

--- internal/models/session.go
+++ internal/models/session.go
@@ -21,6 +21,11 @@ type Session struct {
 	FamilyID   NullUUID    `json:"family_id,omitempty"`
 	IPAtStart  NullString  `json:"ip_at_start,omitempty"`
 	UserAgent  NullString  `json:"user_agent,omitempty"`
+	UserEmail     NullString `json:"user_email,omitempty"`
+	UserFirstName NullString `json:"user_first_name,omitempty"`
+	UserLastName  NullString `json:"user_last_name,omitempty"`
+	FamilyName    NullString `json:"family_name,omitempty"`
+	EnvName       NullString `json:"env_name,omitempty"`
 	CreatedAt  time.Time   `json:"created_at"`
 	LastSeenAt time.Time   `json:"last_seen_at"`
 	RevokedAt  *time.Time  `json:"revoked_at,omitempty"`
